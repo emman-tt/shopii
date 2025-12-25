faf6dd9fb7cc11c0aad2ef974d73dd21bef6b49c
Your Name <Emmanuelaquarius2006@gmail.com>
2025-12-25 02:15:33 +0000

    fixed a little

--- src/reuse/Notification.jsx
+++ src/reuse/Notification.jsx
@@ -15,7 +15,7 @@ export default function ({ message = 'Added To Cart' }) {
   return (
     <section
       ref={box}
-      style={{ display:"flex",justifySelf:"center",alignSelf:"center" }}
+      style={{ display: 'flex', justifySelf: 'center', alignSelf: 'center' }}
       className={`absolute max-sm:fixed w-80     justify-self-center  backdrop-blur-2xl bg-[#17092e] text-white py-5 rounded-2xl gap-8 flex place-content-center top-0 z-120 self-center justify-center sm:w-100 sm:mx-auto  text-xl`}
     >
       <BiCheckCircle color='green' size={30} /> {message}
